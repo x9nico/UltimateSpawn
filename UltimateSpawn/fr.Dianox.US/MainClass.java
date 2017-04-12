5e4dc6a0c3395b28ddfcb0f9b3d433787d2de7ac
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-12 19:23:49 +0200

    Update MainClass.java

--- UltimateSpawn/fr.Dianox.US/MainClass.java
+++ UltimateSpawn/fr.Dianox.US/MainClass.java
@@ -3,6 +3,7 @@ package fr.Dianox.US;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.Dianox.US.Commands.CommandSpawn;
+import fr.Dianox.US.Commands.MainCommand;
 import fr.Dianox.US.config.ConfigMessage;
 import fr.Dianox.US.config.ConfigSpawn;
 import fr.Dianox.US.config.ConfigUtils;
@@ -24,7 +25,7 @@ public class MainClass extends JavaPlugin{
 		
 		getCommand("spawn").setExecutor(new CommandSpawn());
 		getCommand("setspawn").setExecutor(new CommandSpawn());
-		getCommand("ultimatespawn").setExecutor(new CommandSpawn());
+		getCommand("ultimatespawn").setExecutor(new MainCommand());
 		
 		System.out.println("|=============================");
 		System.out.println("|");
