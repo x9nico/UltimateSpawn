da253b9f1339031161bd4433cd375bdcbb4e6115
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-12 19:47:57 +0200

    Fix permission bug

--- UltimateSpawn/fr.Dianox.US/Commands/CommandSpawn.java
+++ UltimateSpawn/fr.Dianox.US/Commands/CommandSpawn.java
@@ -30,27 +30,31 @@ public class CommandSpawn implements CommandExecutor {
 		
 		Player p = (Player)sender;
 		
-		if ((cmd.getName().equalsIgnoreCase("setspawn")) || (cmd.getName().equalsIgnoreCase("sethub")) || (cmd.getName().equalsIgnoreCase("setlobby")) && (p.hasPermission("UltimateSpawn.SetSpawn"))) {
-		      if (!(sender instanceof Player)) {
-		        sender.sendMessage(ConfigUtils.console_use_command);
-		        return true;
-		      }
-		      
-		      Location l = p.getLocation();
-		        
-		      ConfigSpawn.getConfig().set("spawn.world", l.getWorld().getName());
-		      ConfigSpawn.getConfig().set("spawn.x", Double.valueOf(l.getX()));
-		      ConfigSpawn.getConfig().set("spawn.y", Double.valueOf(l.getY()));
-		      ConfigSpawn.getConfig().set("spawn.z", Double.valueOf(l.getZ()));
-		      ConfigSpawn.getConfig().set("spawn.yaw", Float.valueOf(l.getYaw()));
-		      ConfigSpawn.getConfig().set("spawn.pitch", Float.valueOf(l.getPitch()));
-		        
-		      ConfigSpawn.saveConfigFile();
-		        
-		      p.getWorld().setSpawnLocation((int)l.getX(), (int)l.getY(), (int)l.getZ());
-		        
-		      p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("spawn.Spawn-create")));
-		   }
+		if (cmd.getName().equalsIgnoreCase("setspawn") || cmd.getName().equalsIgnoreCase("sethub") || cmd.getName().equalsIgnoreCase("setlobby")) {
+				if(p.hasPermission("UltimateSpawn.SetSpawn")){
+			      if (!(sender instanceof Player)) {
+				        sender.sendMessage(ConfigUtils.console_use_command);
+				        return true;
+				      }
+					      
+				      Location l = p.getLocation();
+					        
+				      ConfigSpawn.getConfig().set("spawn.world", l.getWorld().getName());
+				      ConfigSpawn.getConfig().set("spawn.x", Double.valueOf(l.getX()));
+				      ConfigSpawn.getConfig().set("spawn.y", Double.valueOf(l.getY()));
+				      ConfigSpawn.getConfig().set("spawn.z", Double.valueOf(l.getZ()));
+				      ConfigSpawn.getConfig().set("spawn.yaw", Float.valueOf(l.getYaw()));
+				      ConfigSpawn.getConfig().set("spawn.pitch", Float.valueOf(l.getPitch()));
+					        
+				      ConfigSpawn.saveConfigFile();
+				        
+				      p.getWorld().setSpawnLocation((int)l.getX(), (int)l.getY(), (int)l.getZ());
+				        
+				      p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("spawn.Spawn-create")));
+				} else {
+					sender.sendMessage(ConfigUtils.no_permission);
+				}
+		   } 
 		   else if(label.equalsIgnoreCase("spawn") || label.equalsIgnoreCase("hub") || label.equalsIgnoreCase("lobby") || label.equalsIgnoreCase("h") || label.equalsIgnoreCase("l")){
 			if(args.length == 0){
 		        if (!(sender instanceof Player)) {
