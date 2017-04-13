3b528fe147b78bfe51cf9d85250e91b8fe8c5809
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-13 11:00:47 +0200

    Clean up

--- UltimateSpawn/fr.Dianox.US/config/ConfigUtils.java
+++ UltimateSpawn/fr.Dianox.US/config/ConfigUtils.java
@@ -4,15 +4,15 @@ import org.bukkit.ChatColor;
 
 public class ConfigUtils {
 	
-	  public static String no_permission;
-	  public static String player_not_found;
-	  public static String console_use_command;
+	public static String no_permission;
+	public static String player_not_found;
+	public static String console_use_command;
 	  
-	  public ConfigUtils() {}
+	public ConfigUtils() {}
 	  
-	  public static void load() {
-	    no_permission = ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("others.No-permission-message"));
+	public static void load() {
+		no_permission = ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("others.No-permission-message"));
 	    player_not_found = ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("others.Player-not-found"));
 	    console_use_command = ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("others.Console-use-command"));
-	  }
+	}
 }
