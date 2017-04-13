2f109ecb5a0941d453282e7054ea7bfa5053c721
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-13 13:28:29 +0200

    Add a event to MainClass

--- UltimateSpawn/fr.Dianox.US/MainClass.java
+++ UltimateSpawn/fr.Dianox.US/MainClass.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.Dianox.US.Commands.CommandSpawn;
 import fr.Dianox.US.Commands.MainCommand;
+import fr.Dianox.US.Protection.ProtecBlock;
 import fr.Dianox.US.config.ConfigMessage;
 import fr.Dianox.US.config.ConfigSpawn;
 import fr.Dianox.US.config.ConfigUtils;
@@ -31,6 +32,7 @@ public class MainClass extends JavaPlugin{
 		
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new Events(), this);
+		pm.registerEvents(new ProtecBlock(), this);
 		
 		System.out.println("|=============================");
 		System.out.println("|");
