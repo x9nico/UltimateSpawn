96337d88d3e70e30dfb5ac83840ff6d7a805b83e
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-13 11:00:07 +0200

    Clean up

--- UltimateSpawn/fr.Dianox.US/config/ConfigSpawn.java
+++ UltimateSpawn/fr.Dianox.US/config/ConfigSpawn.java
@@ -8,64 +8,63 @@ import org.bukkit.plugin.Plugin;
 
 public class ConfigSpawn {
 	
-	  private static Plugin pl;
-	  private static File filespawn;
-	  private static YamlConfiguration ConfigSpawn;
+	private static Plugin pl;
+	private static File filespawn;
+	private static YamlConfiguration ConfigSpawn;
 	  
-	  public ConfigSpawn() {}
+	public ConfigSpawn() {}
 	  
-	  public static void loadSpawnConfig(Plugin plugin){
-	    pl = plugin;
+	public static void loadSpawnConfig(Plugin plugin){
+		pl = plugin;
 	    
 	    filespawn = new File(pl.getDataFolder(), "spawn.yml");
 	    ConfigSpawn = YamlConfiguration.loadConfiguration(filespawn);
 	    
 	    if (!pl.getDataFolder().exists()) {
-	      pl.getDataFolder().mkdirs();
+	    	pl.getDataFolder().mkdirs();
 	    }
 	    create();
 	    
 	    saveConfigFile();
-	  }
+	}
 	  
-	  public static File getFile() {
-	    return filespawn;
-	  }
+	public static File getFile() {
+		return filespawn;
+	}
 	  
-	  public static YamlConfiguration getConfig() {
+	public static YamlConfiguration getConfig() {
 	    return ConfigSpawn;
-	  }
+	}
 	  
-	  public static void reloadConfig() {
+	public static void reloadConfig() {
 	    loadSpawnConfig(pl);
-	  }
+	}
 	  
-	  public static void saveConfigFile() {
-	    try {
-	      ConfigSpawn.save(filespawn);
-	    } catch (IOException e) {
-	      e.printStackTrace();
+	public static void saveConfigFile() {
+		try {
+			ConfigSpawn.save(filespawn);
+		} catch (IOException e) {
+			e.printStackTrace();
 	    }
-	  }
+	}
 	  
-	  private static void create() {
-	    if (!filespawn.exists()) {
-	      try {
-	        filespawn.createNewFile();
-	      } catch (IOException e) {
-	        e.printStackTrace();
-	      }
+	private static void create() {
+		if (!filespawn.exists()) {
+			try {
+				filespawn.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 	      
-	      ConfigSpawn.set("spawn.world", null);
-	      ConfigSpawn.set("spawn.x", null);
-	      ConfigSpawn.set("spawn.y", null);
-	      ConfigSpawn.set("spawn.z", null);
-	      ConfigSpawn.set("spawn.yaw", null);
-	      ConfigSpawn.set("spawn.pitch", null);
+			ConfigSpawn.set("spawn.world", null);
+			ConfigSpawn.set("spawn.x", null);
+			ConfigSpawn.set("spawn.y", null);
+			ConfigSpawn.set("spawn.z", null);
+			ConfigSpawn.set("spawn.yaw", null);
+			ConfigSpawn.set("spawn.pitch", null);
 	      
-	      saveConfigFile();
-	    }
-	  }
+			saveConfigFile();
+		}
+	}
 
 }
-
