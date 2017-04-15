903eff97a364d8290b58f67a6a438ab37b919ed8
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-15 12:17:59 +0200

    Update MainClass.java

--- UltimateSpawn/fr.Dianox.US/MainClass.java
+++ UltimateSpawn/fr.Dianox.US/MainClass.java
@@ -7,6 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import fr.Dianox.US.Commands.CommandSpawn;
 import fr.Dianox.US.Commands.MainCommand;
 import fr.Dianox.US.Protection.ProtecBlock;
+import fr.Dianox.US.Protection.ProtectItem;
 import fr.Dianox.US.config.ConfigMessage;
 import fr.Dianox.US.config.ConfigSpawn;
 import fr.Dianox.US.config.ConfigUtils;
@@ -33,6 +34,7 @@ public class MainClass extends JavaPlugin{
 		PluginManager pm = Bukkit.getPluginManager();
 		pm.registerEvents(new Events(), this);
 		pm.registerEvents(new ProtecBlock(), this);
+		pm.registerEvents(new ProtectItem(), this);
 		
 		System.out.println("|=============================");
 		System.out.println("|");
