785dbcca4a54c27627a17e8b2e807efaa2f2f679
DianoxDragon <dianoxcontactpro@gmail.com>
2017-04-22 00:00:25 +0200

    Update MainClass.java

--- UltimateSpawn/fr.Dianox.US/MainClass.java
+++ UltimateSpawn/fr.Dianox.US/MainClass.java
@@ -1,6 +1,7 @@
 package fr.Dianox.US;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,21 +12,26 @@ import fr.Dianox.US.Protection.AntiSpawning;
 import fr.Dianox.US.Protection.AntiWeather;
 import fr.Dianox.US.Protection.ProtecBlock;
 import fr.Dianox.US.Protection.ProtectItem;
+import fr.Dianox.US.Scoreboard.Board;
+import fr.Dianox.US.Scoreboard.Updater;
 import fr.Dianox.US.Protection.ProtectExplosion;
 import fr.Dianox.US.config.ConfigMessage;
 import fr.Dianox.US.config.ConfigSpawn;
 import fr.Dianox.US.config.ConfigUtils;
 import fr.Dianox.US.config.MainConfig;
+import fr.Dianox.US.config.ScoreboardConfig;
 
 public class MainClass extends JavaPlugin{
 	
 	private static MainClass instance;
+	public static boolean placeholders = false;
 	
 	public MainClass(){}
 	
 	public void onEnable(){
 		instance = this;
 		
+		ScoreboardConfig.loadConfig(this);
 		ConfigSpawn.loadSpawnConfig(this);
 		ConfigMessage.loadMessageConfig(this);
 		MainConfig.loadConfig(this);
@@ -44,6 +50,16 @@ public class MainClass extends JavaPlugin{
 		pm.registerEvents(new AntiSpawning(), this);
 		pm.registerEvents(new AntiDamage(), this);
 		
+	    if (Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+	        placeholders = true;
+	    }
+	    
+	    for (Player p : Bukkit.getOnlinePlayers()) {
+	        new Board(p);
+	    }
+	    
+	    new Updater();
+	    
 		System.out.println("|=============================");
 		System.out.println("|");
 		System.out.println("| Ultimate Spawn loaded !");
