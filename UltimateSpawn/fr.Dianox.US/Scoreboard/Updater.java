010b528eb868eeb6d983e10d2f99cfd76dc61a70
DianoxDragon <dianoxcontactpro@gmail.com>
2017-05-03 15:11:04 +0200

    Update Updater.java

--- UltimateSpawn/fr.Dianox.US/Scoreboard/Updater.java
+++ UltimateSpawn/fr.Dianox.US/Scoreboard/Updater.java
@@ -5,12 +5,10 @@ import org.bukkit.Bukkit;
 import fr.Dianox.US.MainClass;
 
 public class Updater {
-	  public Updater()
-	  {
-	    Bukkit.getScheduler().scheduleSyncRepeatingTask(MainClass.getInstance(), new Runnable()
-	    {
-	      public void run()
-	      {
+	
+	  public Updater(){
+	    Bukkit.getScheduler().scheduleSyncRepeatingTask(MainClass.getInstance(), new Runnable(){
+	      public void run(){
 	        for (Board b : Board.boards) {
 	          b.update();
 	        }
