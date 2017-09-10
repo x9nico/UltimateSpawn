473948cd414d9a4b4624ef78475c90732587d28f
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-10 11:51:01 +0200

    Update ConfigMessage.java

--- UltimateSpawn/fr.Dianox.US/MainClass/config/ConfigMessage.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/ConfigMessage.java
@@ -58,14 +58,22 @@ public class ConfigMessage {
 			
 			Config.set("Player.Teleport.To-spawn", "&7Teleport...");
 			Config.set("Player.Teleport.To-spawn-other", "&7Teleport %target%...");
+			Config.set("Player.Ping.Self", "&c%player% &7>> &e%ping%");
+			Config.set("Player.Ping.Other", "&c%target% &7>> &e%ping%");
 			Config.set("Protection.Construct-Message", "&cSorry, you can't construct here !");
 			Config.set("Admin.Plugin-Reload", "&6US &7| &eConfig reloaded &7&oThis command has just reload config.yml and messages.yml");
 			Config.set("Admin.Spawn.Set", "&6US &7| &eSpawn &cset &e!");
+			Config.set("Admin.ClearChat.Anonymously", "&cChat clear");
+			Config.set("Admin.ClearChat.Own", "&cChat clear");
+			Config.set("Admin.ClearChat.Normal", "&cChat clear by %player%");
+			Config.set("Admin.ClearChat.Other", "&cYour chat has been clear");
 			Config.set("Error.Only-Player", "&cSorry, only players can use this command !");
 			Config.set("Error.No-permission", "&cSorry, you don't have the permission !");
-			Config.set("Error.Player-not-found", "&cSorry, but... The player are online ? If it's the case, check the playername");
 			Config.set("Error.Spawn-not-set", "&cSorry, the spawn isn't set");
-
+			Config.set("Error.Command-disable", "&cSorry, this command is disable");
+			Config.set("Error.Player.Not-found", "&cSorry, but... The player are online ? If it's the case, check the playername");
+			Config.set("Error.Player.Enter-Player-Name", "&cThanks to enter a player name");
+			
 			saveConfigFile();
 			
 		}
