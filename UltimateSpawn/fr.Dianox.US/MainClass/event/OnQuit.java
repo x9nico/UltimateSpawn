0f4e27c0a959d6d730c105d665d767eb39ea409a
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-13 18:48:36 +0200

    Update OnQuit.java

--- UltimateSpawn/fr.Dianox.US/MainClass/event/OnQuit.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/event/OnQuit.java
@@ -9,26 +9,26 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import fr.Dianox.US.MainClass.config.ConfigGlobal;
 
 public class OnQuit implements Listener {
-	
-	@EventHandler
-	public void onQuit(PlayerQuitEvent e) {
-		Player p = e.getPlayer();
-		
-		if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Fly")) {
-	    	p.setAllowFlight(false);
-	    	p.setFlying(false);
-	    }
-		
-	    if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Broadcast.Quit.Enable")) {
-	    	if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Broadcast.Quit.Hide")) {
-	    		e.setQuitMessage(null);
-	    	} else {
-	    		for (String message : ConfigGlobal.getConfig().getStringList("On-Join.Spawn.Broadcast.Quit.Message")) {
-		    		p.sendMessage(ChatColor.translateAlternateColorCodes('&', message.replaceAll("%player%", p.getName())));
-	    		}
-	    		e.setQuitMessage(null);
-	    	}
-	    }
-	}
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+
+        if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Fly")) {
+            p.setAllowFlight(false);
+            p.setFlying(false);
+        }
+
+        if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Broadcast.Quit.Enable")) {
+            if (ConfigGlobal.getConfig().getBoolean("On-Join.Spawn.Broadcast.Quit.Hide")) {
+                e.setQuitMessage(null);
+            } else {
+                for (String message: ConfigGlobal.getConfig().getStringList("On-Join.Spawn.Broadcast.Quit.Message")) {
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', message.replaceAll("%player%", p.getName())));
+                }
+                e.setQuitMessage(null);
+            }
+        }
+    }
 
 }
