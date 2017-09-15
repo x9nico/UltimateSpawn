6e419d6d4f8010319cc7632dc02cfc59a88dc938
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-15 21:31:55 +0200

    Update ConfigMessage.java

--- UltimateSpawn/fr.Dianox.US/MainClass/config/ConfigMessage.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/ConfigMessage.java
@@ -55,11 +55,13 @@ public class ConfigMessage {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
+            
             Config.set("Player.Teleport.To-spawn", "&7Teleport...");
             Config.set("Player.Teleport.To-spawn-other", "&7Teleport %target%...");
             Config.set("Player.Ping.Self", "&c%player% &7>> &e%ping%");
             Config.set("Player.Ping.Other", "&c%target% &7>> &e%ping%");
+            Config.set("Player.Chat.Can_t_speak", "&cThe chat is disable");
+            Config.set("Player.Chat.Delay", "&cChat is currently dealyed of %DELAY% seconds");
             Config.set("Protection.Construct-Message", "&cSorry, you can't construct here !");
             Config.set("Admin.Plugin-Reload", "&6US &7| &eConfig reloaded &7&oThis command has just reload config.yml, messages.yml and spawn.yml");
             Config.set("Admin.Spawn.Set", "&6US &7| &eSpawn &cset &e!");
@@ -68,10 +70,16 @@ public class ConfigMessage {
             Config.set("Admin.ClearChat.Normal", "&cChat clear by %player%");
             Config.set("Admin.ClearChat.Other", "&cYour chat has been clear");
             Config.set("Admin.Broadcast", "&8[&eBroadcast&8]&r ");
+            Config.set("Admin.MuteChat.On", "&aGlobal mute has been enabled by %player%!");
+            Config.set("Admin.MuteChat.Off", "&cGlobal mute has been disabled by %player%!");
+            Config.set("Admin.DelayChat.Set", "&cDelay set to &e%DELAY%. &7&oDon't forget to edit this value in the config. Yes this command is only valid if the server does not shut down");
+            Config.set("Admin.MuteChat.Off", "&cGlobal mute has been disabled by %player%!");
             Config.set("Error.Only-Player", "&cSorry, only players can use this command !");
             Config.set("Error.No-permission", "&cSorry, you don't have the permission !");
             Config.set("Error.Spawn-not-set", "&cSorry, the spawn isn't set");
             Config.set("Error.Command-disable", "&cSorry, this command is disable");
+            Config.set("Error.Arguments-Missing", "&cI'm sorry, but there must be one or two arguments missing.");
+            Config.set("Error.Player.Enter-Player-Name", "&cThanks to enter a player name");
             Config.set("Error.Player.Not-found", "&cSorry, but... The player are online ? If it's the case, check the playername");
             Config.set("Error.Player.Enter-Player-Name", "&cThanks to enter a player name");
 
