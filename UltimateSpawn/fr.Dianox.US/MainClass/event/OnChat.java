674b1a761eb3cfd61a186911abba9d1347c3bde2
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-16 20:31:44 +0200

    Update OnChat.java

--- UltimateSpawn/fr.Dianox.US/MainClass/event/OnChat.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/event/OnChat.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +11,7 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 import fr.Dianox.US.MainClass.MainClass;
 import fr.Dianox.US.MainClass.Commands.Chat.DelaychatCommand;
+import fr.Dianox.US.MainClass.Utils.PlaceHolderMessageUtils;
 import fr.Dianox.US.MainClass.config.ConfigGlobal;
 import fr.Dianox.US.MainClass.config.ConfigMessage;
 
@@ -30,11 +30,11 @@ public class OnChat implements Listener {
 			if (ConfigGlobal.getConfig().getBoolean("Command.MuteChat.Mute.Bypass")) {
 				if (!p.hasPermission("UltimateSpawn.bypass.MuteChat")) {
 					e.setCancelled(true);
-					p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Player.Chat.Can_t_speak").replaceAll("%player%", e.getPlayer().getName())));
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Player.Chat.Can_t_speak"), p);
 				}
 			} else {
 				e.setCancelled(true);
-				p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Player.Chat.Can_t_speak").replaceAll("%player%", e.getPlayer().getName())));
+				PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Player.Chat.Can_t_speak"), p);
 			}
 		}
 		
@@ -43,8 +43,8 @@ public class OnChat implements Listener {
 				if (!p.hasPermission("UltimateSpawn.bypass.ChatDelay")) {
 					if (cooling.contains(name)) {
 				    	e.setCancelled(true);
-				        p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Player.Chat.Delay").replaceAll("%DELAY%", String.valueOf(DelaychatCommand.delay))));
-				      } else {
+				        PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Player.Chat.Delay"), p);
+					} else {
 				        cooling.add(name);
 				        
 				        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MainClass.getInstance(), new Runnable() {
@@ -57,7 +57,7 @@ public class OnChat implements Listener {
 			} else {
 				if (cooling.contains(name)) {
 			    	e.setCancelled(true);
-			        p.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Player.Chat.Delay").replaceAll("%DELAY%", String.valueOf(DelaychatCommand.delay))));
+			    	PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Player.Chat.Delay"), p);
 			      } else {
 			        cooling.add(name);
 			        
