8a309e2d62a0e7f7882cf51ab4740c04288e8c21
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-20 20:19:06 +0200

    Update SpawnUtils.java

--- UltimateSpawn/fr.Dianox.US/MainClass/Utils/SpawnUtils.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Utils/SpawnUtils.java
@@ -3,6 +3,7 @@ package fr.Dianox.US.MainClass.Utils;
 import org.bukkit.entity.Player;
 
 import fr.Dianox.US.MainClass.config.ConfigMessage;
+import fr.Dianox.US.MainClass.config.ConfigPlayerStats;
 import fr.Dianox.US.MainClass.config.ConfigSpawn;
 
 public class SpawnUtils {
@@ -21,6 +22,21 @@ public class SpawnUtils {
             org.bukkit.Bukkit.getLogger().warning("UltimateSpawn : Spawn is not set");
 
             PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Spawn-not-set"), player);
+        }
+    }
+    
+    public static void teleportToSpawnStats(Player player) {
+        try {
+            org.bukkit.World w = org.bukkit.Bukkit.getServer().getWorld(ConfigPlayerStats.getConfig().getString(player.getUniqueId()+".Position.Last_logout.World"));
+            double x = ConfigPlayerStats.getConfig().getDouble(player.getUniqueId()+".Position.Last_logout.x");
+            double y = ConfigPlayerStats.getConfig().getDouble(player.getUniqueId()+".Position.Last_logout.y");
+            double z = ConfigPlayerStats.getConfig().getDouble(player.getUniqueId()+".Position.Last_logout.z");
+            float yaw = ConfigPlayerStats.getConfig().getInt(player.getUniqueId()+".Position.Last_logout.yaw");
+            float pitch = ConfigPlayerStats.getConfig().getInt(player.getUniqueId()+".Position.Last_logout.pitch");
+
+            player.teleport(new org.bukkit.Location(w, x, y, z, yaw, pitch));
+        } catch (Exception e) {
+            org.bukkit.Bukkit.getLogger().warning("UltimateSpawn : Error either in the plugin or in your configuration");
         }
     }
 
