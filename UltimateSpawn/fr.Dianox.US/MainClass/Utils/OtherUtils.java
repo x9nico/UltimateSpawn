5d659e5f4500fb0ef5af99d6bf604bb3a37bfabb
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-27 18:29:19 +0200

    Update OtherUtils.java

--- UltimateSpawn/fr.Dianox.US/MainClass/Utils/OtherUtils.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Utils/OtherUtils.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import org.bukkit.Color;
+
 import fr.Dianox.US.MainClass.config.ConfigGlobal;
 
 public class OtherUtils {
@@ -36,5 +38,62 @@ public class OtherUtils {
 		
 		return seconds;
 	}
+	
+	  public static Color getColor(String paramString) {
+	    String temp = paramString;
+	    if (temp.equalsIgnoreCase("AQUA")) {
+	      return Color.AQUA;
+	    }
+	    if (temp.equalsIgnoreCase("BLACK")) {
+	      return Color.BLACK;
+	    }
+	    if (temp.equalsIgnoreCase("BLUE")) {
+	      return Color.BLUE;
+	    }
+	    if (temp.equalsIgnoreCase("FUCHSIA")) {
+	      return Color.FUCHSIA;
+	    }
+	    if (temp.equalsIgnoreCase("GRAY")) {
+	      return Color.GRAY;
+	    }
+	    if (temp.equalsIgnoreCase("GREEN")) {
+	      return Color.GREEN;
+	    }
+	    if (temp.equalsIgnoreCase("LIME")) {
+	      return Color.LIME;
+	    }
+	    if (temp.equalsIgnoreCase("MAROON")) {
+	      return Color.MAROON;
+	    }
+	    if (temp.equalsIgnoreCase("NAVY")) {
+	      return Color.NAVY;
+	    }
+	    if (temp.equalsIgnoreCase("OLIVE")) {
+	      return Color.OLIVE;
+	    }
+	    if (temp.equalsIgnoreCase("ORANGE")) {
+	      return Color.ORANGE;
+	    }
+	    if (temp.equalsIgnoreCase("PURPLE")) {
+	      return Color.PURPLE;
+	    }
+	    if (temp.equalsIgnoreCase("RED")) {
+	      return Color.RED;
+	    }
+	    if (temp.equalsIgnoreCase("SILVER")) {
+	      return Color.SILVER;
+	    }
+	    if (temp.equalsIgnoreCase("TEAL")) {
+	      return Color.TEAL;
+	    }
+	    if (temp.equalsIgnoreCase("WHITE")) {
+	      return Color.WHITE;
+	    }
+	    if (temp.equalsIgnoreCase("YELLOW")) {
+	      return Color.YELLOW;
+	    }
+	    
+	    return null;
+	  }
 
 }
