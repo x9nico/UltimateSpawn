c9e08915f9275aaca2c82974a2b3c691d65fa345
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-27 15:21:27 +0200

    Create ConfigGMessageQ.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/global/ConfigGMessageQ.java
@@ -0,0 +1,75 @@
+package fr.Dianox.US.MainClass.config.global;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigGMessageQ {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigGMessageQ() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Global/OnQuit/Message-OnJoin.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Config.set("Broadcast.Quit.Enable", Boolean.valueOf(true));
+            Config.set("Broadcast.Quit.Hide", Boolean.valueOf(false));
+            Config.set("Broadcast.Quit.Message", java.util.Arrays.asList(new String[] {
+                "&8[&c-&8] [&c-&8] [&c-&8] ",
+                "&cHey, %player% :'("
+            }));
+            Config.set("Broadcast.Quit.World.All_World", Boolean.valueOf(false));
+            Config.set("Broadcast.Quit.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            
+            saveConfigFile();
+
+        }
+    }
+}
