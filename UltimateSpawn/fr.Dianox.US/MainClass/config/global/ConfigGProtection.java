45969a72dfec4ef1332e80765af38756a8a77e35
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-27 17:55:56 +0200

    Create ConfigGProtection.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/global/ConfigGProtection.java
@@ -0,0 +1,81 @@
+package fr.Dianox.US.MainClass.config.global;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigGProtection {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigGProtection() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Global/Server-Protection.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            // Construct
+            Config.set("Protection.Construct.Place.Enable", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Place.Bypass", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Place.Message", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Place.World.All_World", Boolean.valueOf(false));
+            Config.set("Protection.Construct.Place.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            
+            Config.set("Protection.Construct.Break.Enable", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Break.Bypass", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Break.Message", Boolean.valueOf(true));
+            Config.set("Protection.Construct.Break.World.All_World", Boolean.valueOf(false));
+            Config.set("Protection.Construct.Break.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            
+            saveConfigFile();
+
+        }
+    }
+}
