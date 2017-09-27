8dc06e6f4f556a8bd84a98638960a0ab91674b31
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-27 14:58:24 +0200

    Create ConfigGSpawn.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/global/ConfigGSpawn.java
@@ -0,0 +1,66 @@
+package fr.Dianox.US.MainClass.config.global;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigGSpawn {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigGSpawn() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Global/OnJoin/Spawn-OnJoin.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            Config.set("Spawn.Teleport.Enable", Boolean.valueOf(true));
+            Config.set("Spawn.Teleport.If_stats_is_enable.Use_stats_Of_UltimateSpawn.To_teleport_players.To_their_last_location", Boolean.valueOf(false));
+            Config.set("Spawn.Teleport.On-First-Join", Boolean.valueOf(true));
+            
+            saveConfigFile();
+
+        }
+    }
+}
