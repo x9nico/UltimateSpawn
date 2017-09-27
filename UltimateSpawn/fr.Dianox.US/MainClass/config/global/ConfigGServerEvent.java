137546090dcc6529f82194a17f3992dd3a23a927
DianoxDragon <dianoxcontactpro@gmail.com>
2017-09-27 15:36:32 +0200

    Create ConfigGServerEvent.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/global/ConfigGServerEvent.java
@@ -0,0 +1,106 @@
+package fr.Dianox.US.MainClass.config.global;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigGServerEvent {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigGServerEvent() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Global/Server-Event.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            //Server
+            Config.set("Server.Disable.Hunger.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Hunger.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Hunger.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Damage.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Damage.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Damage.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Weather.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Weather.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Weather.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Burn-block.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Burn-block.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Burn-block.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Explosion.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Explosion.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Explosion.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Death-Message.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Death-Message.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Death-Message.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            Config.set("Server.Disable.Spawning-Monster-Animals.Enable", Boolean.valueOf(true));
+            Config.set("Server.Disable.Spawning-Monster-Animals.World.All_World", Boolean.valueOf(false));
+            Config.set("Server.Disable.Spawning-Monster-Animals.World.Worlds", java.util.Arrays.asList(new String[] {
+                    "world",
+                    "world_nether"
+                }));
+            
+            saveConfigFile();
+
+        }
+    }
+}
