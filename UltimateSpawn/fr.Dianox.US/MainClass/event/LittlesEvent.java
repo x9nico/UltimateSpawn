43698c4a2bd8a55792ae3cbaf39e08df88f4dd25
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-06 13:31:06 +0200

    Update LittlesEvent.java

--- UltimateSpawn/fr.Dianox.US/MainClass/event/LittlesEvent.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/event/LittlesEvent.java
@@ -1,12 +1,17 @@
 package fr.Dianox.US.MainClass.event;
 
+import java.util.UUID;
+
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 
 import fr.Dianox.US.MainClass.MainClass;
+import fr.Dianox.US.MainClass.config.ConfigTemp;
 import fr.Dianox.US.MainClass.config.event.ConfigEColorSign;
 
 public class LittlesEvent implements Listener {
@@ -40,6 +45,36 @@ public class LittlesEvent implements Listener {
 				}
 			}
 		}
+	}
+	
+	@EventHandler
+	public void onChangeGM(PlayerGameModeChangeEvent e) {
+		Player p = e.getPlayer();
+        UUID pU = e.getPlayer().getUniqueId();
+        
+		int GamemodeTemp1 = 0; 
+        if (e.getNewGameMode() == GameMode.SURVIVAL) {
+        	GamemodeTemp1 = 0;
+        } else if (e.getNewGameMode() == GameMode.CREATIVE) {
+        	GamemodeTemp1 = 1;
+        } else if (e.getNewGameMode() == GameMode.ADVENTURE) {
+        	GamemodeTemp1 = 2;
+        } else if (e.getNewGameMode() == GameMode.SPECTATOR) {
+        	GamemodeTemp1 = 3;
+        }
+        int GamemodeTemp2 = Integer.valueOf(GamemodeTemp1);
+        
+        if (!ConfigTemp.getConfig().contains(String.valueOf(pU))) {
+        	ConfigTemp.getConfig().set(pU+".Player", String.valueOf(p));
+        	ConfigTemp.getConfig().set(pU+".Options.Gamemode", Integer.valueOf(GamemodeTemp2));
+        	
+        	ConfigTemp.saveConfigFile();
+        } else if (ConfigTemp.getConfig().contains(String.valueOf(pU))) {
+        	ConfigTemp.getConfig().set(pU+".Player", String.valueOf(p));
+        	ConfigTemp.getConfig().set(pU+".Options.Gamemode", Integer.valueOf(GamemodeTemp2));
+        	
+        	ConfigTemp.saveConfigFile();
+        }
 	}
 
 }
