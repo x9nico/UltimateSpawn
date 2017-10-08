f4390d5f6c54e0ad91ba660142ef04810bef6eae
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-08 12:01:01 +0200

    Update BasicFeatures.java

--- UltimateSpawn/fr.Dianox.US/MainClass/event/BasicFeatures.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/event/BasicFeatures.java
@@ -32,9 +32,9 @@ import org.bukkit.event.weather.LightningStrikeEvent;
 import org.bukkit.event.weather.ThunderChangeEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
-import fr.Dianox.US.MainClass.MainClass;
 import fr.Dianox.US.MainClass.Utils.PlaceHolderMessageUtils;
 import fr.Dianox.US.MainClass.Utils.SpawnUtils;
+import fr.Dianox.US.MainClass.Utils.WorldUtils;
 import fr.Dianox.US.MainClass.config.ConfigGlobal;
 import fr.Dianox.US.MainClass.config.ConfigMessage;
 import fr.Dianox.US.MainClass.config.event.ConfigEVoidTP;
@@ -49,7 +49,7 @@ public class BasicFeatures implements Listener {
     public void foodChangeLevel(FoodLevelChangeEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Hunger.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Hunger.World.All_World")) {
-    			if (MainClass.getWH().contains(e.getEntity().getLocation().getWorld().getName())) {
+    			if (WorldUtils.getWH().contains(e.getEntity().getLocation().getWorld().getName())) {
 			        if (e.getEntityType() == EntityType.PLAYER) {
 			            Player p = (Player) e.getEntity();
 			            	
@@ -79,7 +79,7 @@ public class BasicFeatures implements Listener {
     public void DisableDamage(EntityDamageEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Damage.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Damage.World.All_World")) {
-		    	if (MainClass.getWD().contains(e.getEntity().getLocation().getWorld().getName())) {
+		    	if (WorldUtils.getWD().contains(e.getEntity().getLocation().getWorld().getName())) {
 		    		if (ConfigGlobal.getConfig().getBoolean("Debug-in-case-of-problem.Disable.Damage-TO-EVERYTHING")) {
 		    			e.setCancelled(true);
 		    		} else {
@@ -681,7 +681,7 @@ public class BasicFeatures implements Listener {
     public void WeatherDisable(WeatherChangeEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Weather.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Weather.World.All_World")) {
-    			if (MainClass.getWW().contains(e.getWorld().getName())) {
+    			if (WorldUtils.getWW().contains(e.getWorld().getName())) {
     				e.setCancelled(true);
     			}
     		} else {
@@ -695,7 +695,7 @@ public class BasicFeatures implements Listener {
     public void Thunder(ThunderChangeEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.ThunderChange.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.ThunderChange.World.All_World")) {
-    			if (MainClass.getWTC().contains(e.getWorld().getName())) {
+    			if (WorldUtils.getWTC().contains(e.getWorld().getName())) {
     				e.setCancelled(true);
     			}
     		} else {
@@ -711,7 +711,7 @@ public class BasicFeatures implements Listener {
 
         if (ConfigGProtection.getConfig().getBoolean("Protection.Construct.Place.Enable")) {
         	if (!ConfigGProtection.getConfig().getBoolean("Protection.Construct.Place.World.All_World")) {
-        		if (MainClass.getWPCP().contains(p.getWorld().getName())) {
+        		if (WorldUtils.getWPCP().contains(p.getWorld().getName())) {
         			if (ConfigGProtection.getConfig().getBoolean("Protection.Construct.Place.Bypass")) {
         		        if (!p.hasPermission("UltimateSpawn.bypass.ConstructPlace")) {
         		            e.setCancelled(true);
@@ -750,7 +750,7 @@ public class BasicFeatures implements Listener {
 
         if (ConfigGProtection.getConfig().getBoolean("Protection.Construct.Break.Enable")) {
         	if (!ConfigGProtection.getConfig().getBoolean("Protection.Construct.Break.World.All_World")) {
-        		if (MainClass.getWPCB().contains(p.getWorld().getName())) {
+        		if (WorldUtils.getWPCB().contains(p.getWorld().getName())) {
         			if (ConfigGProtection.getConfig().getBoolean("Protection.Construct.Break.Bypass")) {
         		        if (!p.hasPermission("UltimateSpawn.bypass.ConstructBreak")) {
         		            e.setCancelled(true);
@@ -787,7 +787,7 @@ public class BasicFeatures implements Listener {
     public void onHanging(HangingBreakByEntityEvent e) {
     	if (ConfigGProtection.getConfig().getBoolean("Protection.HagingBreakByEntity.Enable")) {
     		if (!ConfigGProtection.getConfig().getBoolean("Protection.HagingBreakByEntity.World.All_World")) {
-    			if (MainClass.getWHBBE().contains(e.getEntity().getWorld().getName())) {
+    			if (WorldUtils.getWHBBE().contains(e.getEntity().getWorld().getName())) {
     				e.setCancelled(true);
     			}
     		} else {
@@ -801,7 +801,7 @@ public class BasicFeatures implements Listener {
     	Player p = e.getPlayer();
     	if (ConfigGProtection.getConfig().getBoolean("Protection.PlayerInteractEntity-ItemFrame.Enable")) {
     		if (!ConfigGProtection.getConfig().getBoolean("Protection.PlayerInteractEntity-ItemFrame.World.All_World")) {
-    			if (MainClass.getWPIEIF().contains(p.getWorld().getName())) {
+    			if (WorldUtils.getWPIEIF().contains(p.getWorld().getName())) {
     				if ((e.getRightClicked() instanceof ItemFrame)) {
     			        e.setCancelled(true);
     				}
@@ -819,7 +819,7 @@ public class BasicFeatures implements Listener {
     public void BlockBurn(BlockBurnEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Burn-block.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Burn-block.World.All_World")) {
-    			if (MainClass.getWBB().contains(e.getBlock().getWorld().getName())) {
+    			if (WorldUtils.getWBB().contains(e.getBlock().getWorld().getName())) {
     				e.setCancelled(true);
     			}
     		} else {
@@ -832,7 +832,7 @@ public class BasicFeatures implements Listener {
     public void FireSpread(BlockIgniteEvent e) {
     	if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.BlockIgnite-FireSpread.Enable")) {
     		if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.BlockIgnite-FireSpread.World.All_World")) {
-    			if (MainClass.getWFS().contains(e.getBlock().getWorld().getName())) {
+    			if (WorldUtils.getWFS().contains(e.getBlock().getWorld().getName())) {
     				if (e.getCause() == IgniteCause.SPREAD) {
     					e.setCancelled(true);
     				}
@@ -852,7 +852,7 @@ public class BasicFeatures implements Listener {
 
     	if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Drop.Disable")) {
     		if (!ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Drop.World.All_World")) {
-    			if (MainClass.getWItemDrop().contains(p.getWorld().getName())) {
+    			if (WorldUtils.getWItemDrop().contains(p.getWorld().getName())) {
 	    			if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Drop.Bypass")) {
 	                    if (!p.hasPermission("UltimateSpawn.bypass.DropItem")) {
 	                        e.setCancelled(true);
@@ -879,7 +879,7 @@ public class BasicFeatures implements Listener {
         
         if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.PickUp.Disable")) {
     		if (!ConfigGPlayerItems.getConfig().getBoolean("Server.Items.PickUp.World.All_World")) {
-    			if (MainClass.getWItemPickUp().contains(p.getWorld().getName())) {
+    			if (WorldUtils.getWItemPickUp().contains(p.getWorld().getName())) {
 	    			if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.PickUp.Bypass")) {
 	                    if (!p.hasPermission("UltimateSpawn.bypass.PickUpItem")) {
 	                        e.setCancelled(true);
@@ -906,7 +906,7 @@ public class BasicFeatures implements Listener {
         
         if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Move.Disable")) {
     		if (!ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Move.World.All_World")) {
-    			if (MainClass.getWMoveItem().contains(p.getWorld().getName())) {
+    			if (WorldUtils.getWMoveItem().contains(p.getWorld().getName())) {
 	    			if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Move.Bypass")) {
 	                    if (!p.hasPermission("UltimateSpawn.bypass.MoveItem")) {
 	                        e.setCancelled(true);
@@ -933,7 +933,7 @@ public class BasicFeatures implements Listener {
         
         if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Damage-Item.Disable")) {
     		if (!ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Damage-Item.World.All_World")) {
-    			if (MainClass.getWItemDamage().contains(p.getWorld().getName())) {
+    			if (WorldUtils.getWItemDamage().contains(p.getWorld().getName())) {
 	    			if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Damage-Item.Bypass")) {
 	                    if (!p.hasPermission("UltimateSpawn.bypass.DamageItem")) {
 	                        e.setCancelled(true);
@@ -959,7 +959,7 @@ public class BasicFeatures implements Listener {
     public void onExplode(ExplosionPrimeEvent e) {
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Explosion.Enable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Explosion.World.All_World")) {
-        		if (MainClass.getWE().contains(e.getEntity().getWorld().getName())) {
+        		if (WorldUtils.getWE().contains(e.getEntity().getWorld().getName())) {
         			e.setCancelled(true);
         		}
         	} else {
@@ -975,7 +975,7 @@ public class BasicFeatures implements Listener {
         
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Death-Message.Enable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Death-Message.World.All_World")) {
-        		if (MainClass.getWDM().contains(e.getEntity().getWorld().getName())) {
+        		if (WorldUtils.getWDM().contains(e.getEntity().getWorld().getName())) {
         			e.setDeathMessage(null);
         		}
         	} else {
@@ -985,7 +985,7 @@ public class BasicFeatures implements Listener {
 
         if (ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Clear-Drops-On-Death.Enable")) {
         	if (!ConfigGPlayerItems.getConfig().getBoolean("Server.Items.Clear-Drops-On-Death.World.All_World")) {
-        		if (MainClass.getWClearOnDropsOnDeath().contains(e.getEntity().getWorld().getName())) {
+        		if (WorldUtils.getWClearOnDropsOnDeath().contains(e.getEntity().getWorld().getName())) {
         			if (ConfigGlobal.getConfig().getBoolean("Server.Items.Clear-Drops-On-Death.Bypass")) {
                         if (!p.hasPermission("UltimateSpawn.bypass.ClearDropOnDeath")) {
                             if ((e.getEntity() instanceof Player)) {
@@ -1027,7 +1027,7 @@ public class BasicFeatures implements Listener {
     public void onSpawning(CreatureSpawnEvent e) {
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Spawning-Monster-Animals.Enable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Spawning-Monster-Animals.World.All_World")) {
-        		if (MainClass.getWSMA().contains(e.getEntity().getWorld().getName())) {
+        		if (WorldUtils.getWSMA().contains(e.getEntity().getWorld().getName())) {
         			e.setCancelled(true);
         		}
         	} else {
@@ -1041,7 +1041,7 @@ public class BasicFeatures implements Listener {
     public void onLeaveDecay(LeavesDecayEvent e) {
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Leave-Decay.Disable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Leave-Decay.World.All_World")) {
-        		if (MainClass.getWLD().contains(e.getBlock().getWorld().getName())) {
+        		if (WorldUtils.getWLD().contains(e.getBlock().getWorld().getName())) {
         			e.setCancelled(true);
         		}
         	} else {
@@ -1055,7 +1055,7 @@ public class BasicFeatures implements Listener {
     public void onLightNing(LightningStrikeEvent e) {
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.LightningStrike.Disable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.LightningStrike.World.All_World")) {
-        		if (MainClass.getWLS().contains(e.getLightning().getWorld().getName())) {
+        		if (WorldUtils.getWLS().contains(e.getLightning().getWorld().getName())) {
         			e.setCancelled(true);
         		}
         	} else {
@@ -1069,7 +1069,7 @@ public class BasicFeatures implements Listener {
     public void onBlockFade(BlockFadeEvent e) {
         if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Block-Fade.Disable")) {
         	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Block-Fade.World.All_World")) {
-        		if (MainClass.getWBF().contains(e.getBlock().getWorld().getName())) {
+        		if (WorldUtils.getWBF().contains(e.getBlock().getWorld().getName())) {
         			e.setCancelled(true);
         		}
         	} else {
@@ -1085,7 +1085,7 @@ public class BasicFeatures implements Listener {
     		if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Damage.Options.Entity.Enable")) {
 		        if (ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Damage.Options.Entity.EntityDamageByEntity")) {
 		        	if (!ConfigGServerEvent.getConfig().getBoolean("Server.Disable.Damage.World.All_World")) {
-		        		if (MainClass.getWD().contains(e.getEntity().getWorld().getName())) {
+		        		if (WorldUtils.getWD().contains(e.getEntity().getWorld().getName())) {
 		        			e.setCancelled(true);
 		        			if (e.getEntity() instanceof ItemFrame) {
 		        			      e.setCancelled(true);
@@ -1111,7 +1111,7 @@ public class BasicFeatures implements Listener {
     	
         if (ConfigEVoidTP.getConfig().getBoolean("VoidTP.Enable")) {
         	if (!ConfigEVoidTP.getConfig().getBoolean("VoidTP.World.All_World")) {
-        		if (MainClass.getWVoidTP().contains(p.getWorld().getName())) {
+        		if (WorldUtils.getWVoidTP().contains(p.getWorld().getName())) {
         			if(loc.getY() <= getYConfig) {
         				SpawnUtils.teleportToVoidTP(p);
         				if (ConfigEVoidTP.getConfig().getBoolean("VoidTP.Options.Message.Custom")) {
