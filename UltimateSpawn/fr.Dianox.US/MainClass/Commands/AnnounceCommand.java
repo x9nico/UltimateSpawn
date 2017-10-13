d01aa05bfcd4b1b12203ae7e3201f30eb96f5ee3
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-13 13:31:39 +0200

    Update AnnounceCommand.java

--- UltimateSpawn/fr.Dianox.US/MainClass/Commands/AnnounceCommand.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Commands/AnnounceCommand.java
@@ -12,7 +12,7 @@ import org.bukkit.entity.Player;
 
 import fr.Dianox.US.MainClass.Utils.PlaceHolderMessageUtils;
 import fr.Dianox.US.MainClass.config.ConfigMessage;
-import fr.Dianox.US.MainClass.config.command.ConfigCGlobal;
+import fr.Dianox.US.MainClass.config.command.ConfigCAnnounce;
 
 public class AnnounceCommand implements CommandExecutor {
 
@@ -20,12 +20,10 @@ public class AnnounceCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
 
         if (!(sender instanceof Player)) {
-
             if (args.length == 0) {
             	sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Error.Arguments-Missing")));
                 return true;
             }
-
             String msg = "";
             for (String s: args) {
                 if (!Objects.equals(msg, "")) {
@@ -33,65 +31,56 @@ public class AnnounceCommand implements CommandExecutor {
                 }
                 msg = msg + s;
             }
-            
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Console.Broadcast")+msg));
-
             PlaceHolderMessageUtils.ReplaceCharBroadcastPlayer(ConfigMessage.getConfig().getString("Admin.Broadcast") + msg, Bukkit.getServer());
-            
             MusicConsole();
-
             return true;
         }
 
         Player p = (Player) sender;
 
-        if (ConfigCGlobal.getConfig().getBoolean("Command.Broadcast.Enable")) {
-            if ((cmd.getName().equalsIgnoreCase("bc") || cmd.getName().equalsIgnoreCase("broadcast")) && p.hasPermission("UltimateSpawn.Broadcast")) {
-
-                if (args.length == 0) {
-                	PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Arguments-Missing"), p);
-                    return true;
+        if (ConfigCAnnounce.getConfig().getBoolean("Announce.Broadcast.Enable")) {
+            if (cmd.getName().equalsIgnoreCase("bc") || cmd.getName().equalsIgnoreCase("broadcast")) {
+            	if (p.hasPermission("ultimatespawn.command.broadcast")) {
+	                if (args.length == 0) {
+	                	PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Arguments-Missing"), p);
+	                    return true;
+	                }
+	                String msg = "";
+	                for (String s: args) {
+	                    if (!Objects.equals(msg, "")) {
+	                        msg = msg + " ";
+	                    }
+	                    msg = msg + s;
+	                }
+	                Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Console.Broadcast")+msg));
+	                PlaceHolderMessageUtils.ReplaceCharBroadcastPlayer(ConfigMessage.getConfig().getString("Admin.Broadcast") + msg, Bukkit.getServer());
+	                if (ConfigCAnnounce.getConfig().getBoolean("Announce.Broadcast.Sounds.Enabled")) {
+	                    for (Player player: Bukkit.getServer().getOnlinePlayers()) {
+	                        String sound = ConfigCAnnounce.getConfig().getString("Announce.Broadcast.Sounds.Sound");
+	                        int volume = ConfigCAnnounce.getConfig().getInt("Announce.Broadcast.Sounds.Volume");
+	                        int pitch = ConfigCAnnounce.getConfig().getInt("Announce.Broadcast.Sounds.Pitch");
+	                        player.playSound(player.getLocation(), Sound.valueOf(sound), volume, pitch);
+	                    }
+	                }
+            	} else {
+                    PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.No-permission"), p);
                 }
-
-                String msg = "";
-                for (String s: args) {
-                    if (!Objects.equals(msg, "")) {
-                        msg = msg + " ";
-                    }
-                    msg = msg + s;
-                }
-
-                Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', ConfigMessage.getConfig().getString("Console.Broadcast")+msg));
-
-                PlaceHolderMessageUtils.ReplaceCharBroadcastPlayer(ConfigMessage.getConfig().getString("Admin.Broadcast") + msg, Bukkit.getServer());
-                
-                if (ConfigCGlobal.getConfig().getBoolean("Command.Broadcast.Sounds.Enabled")) {
-                    for (Player player: Bukkit.getServer().getOnlinePlayers()) {
-                        String sound = ConfigCGlobal.getConfig().getString("Command.Broadcast.Sounds.Sound");
-                        int volume = ConfigCGlobal.getConfig().getInt("Command.Broadcast.Sounds.Volume");
-                        int pitch = ConfigCGlobal.getConfig().getInt("Command.Broadcast.Sounds.Pitch");
-                        player.playSound(player.getLocation(), Sound.valueOf(sound), volume, pitch);
-                    }
-                }
-
-            } else {
-                PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.No-permission"), p);
             }
         } else {
-            if (ConfigCGlobal.getConfig().getBoolean("Command.Broadcast.Disable-Message")) {
+            if (ConfigCAnnounce.getConfig().getBoolean("Announce.Broadcast.Disable-Message")) {
                 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
             }
         }
-
         return true;
     }
 
     private static void MusicConsole() {
-        if (ConfigCGlobal.getConfig().getBoolean("Command.Broadcast.Sounds-Console.Enabled")) {
+        if (ConfigCAnnounce.getConfig().getBoolean("Announce.Broadcast.Sounds-Console.Enabled")) {
             for (Player player: Bukkit.getServer().getOnlinePlayers()) {
-                String sound = ConfigCGlobal.getConfig().getString("Command.Broadcast.Sounds-Console.Sound");
-                int volume = ConfigCGlobal.getConfig().getInt("Command.Broadcast.Sounds-Console.Volume");
-                int pitch = ConfigCGlobal.getConfig().getInt("Command.Broadcast.Sounds-Console.Pitch");
+                String sound = ConfigCAnnounce.getConfig().getString("Announce.Broadcast.Sounds-Console.Sound");
+                int volume = ConfigCAnnounce.getConfig().getInt("Announce.Broadcast.Sounds-Console.Volume");
+                int pitch = ConfigCAnnounce.getConfig().getInt("Announce.Broadcast.Sounds-Console.Pitch");
                 player.playSound(player.getLocation(), Sound.valueOf(sound), volume, pitch);
             }
         }
