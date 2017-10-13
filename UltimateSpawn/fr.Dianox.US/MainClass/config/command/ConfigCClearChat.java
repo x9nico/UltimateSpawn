686c5ab7c34a79e6580b4fcf115f934a474fb364
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-13 16:07:52 +0200

    Create ConfigCClearChat.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/command/ConfigCClearChat.java
@@ -0,0 +1,84 @@
+package fr.Dianox.US.MainClass.config.command;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigCClearChat {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigCClearChat() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Command/Chat/ClearChat.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            Config.set("ClearChat.Enable", Boolean.valueOf(true));
+            Config.set("ClearChat.Lines-To-Clear", Integer.valueOf(150));
+            Config.set("ClearChat.Disable-Message", Boolean.valueOf(true));
+            Config.set("ClearChat.Anonymous.Enable", Boolean.valueOf(true));
+            Config.set("ClearChat.Anonymous.Message-Clear", Boolean.valueOf(true));
+            Config.set("ClearChat.Anonymous.Use_Permission", Boolean.valueOf(true));
+            Config.set("ClearChat.Anonymous.Disable-Message", Boolean.valueOf(true));
+            Config.set("ClearChat.Normal.Enable", Boolean.valueOf(true));
+            Config.set("ClearChat.Normal.Message-Clear", Boolean.valueOf(true));
+            Config.set("ClearChat.Normal.Use_Permission", Boolean.valueOf(true));
+            Config.set("ClearChat.Normal.Disable-Message", Boolean.valueOf(true));
+            Config.set("ClearChat.Own.Enable", Boolean.valueOf(true));
+            Config.set("ClearChat.Own.Message-Clear", Boolean.valueOf(true));
+            Config.set("ClearChat.Own.Use_Permission", Boolean.valueOf(false));
+            Config.set("ClearChat.Own.Disable-Message", Boolean.valueOf(true));
+            Config.set("ClearChat.Other.Enable", Boolean.valueOf(true));
+            Config.set("ClearChat.Other.Message-Clear", Boolean.valueOf(true));
+            Config.set("ClearChat.Other.Use_Permission", Boolean.valueOf(false));
+            Config.set("ClearChat.Other.Disable-Message", Boolean.valueOf(true));
+            Config.set("ClearChat.Console.Anonymous-Message-Clear", Boolean.valueOf(true));
+            Config.set("ClearChat.Console.Normal-Message-Clear", Boolean.valueOf(true));
+            
+            saveConfigFile();
+
+        }
+    }
+}
