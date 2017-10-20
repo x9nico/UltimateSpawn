0eebd801ebf96d383ebf578fd8ba2c6f32ae1055
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-20 23:55:20 +0200

    Create ConfigCPlayerOption.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/config/command/ConfigCPlayerOption.java
@@ -0,0 +1,70 @@
+package fr.Dianox.US.MainClass.config.command;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigCPlayerOption {
+
+    private static Plugin pl;
+    private static File file;
+    private static YamlConfiguration Config;
+
+    public ConfigCPlayerOption() {}
+
+    public static void loadConfig(Plugin plugin) {
+        pl = plugin;
+
+        file = new File(pl.getDataFolder(), "Config/Command/PlayerOption.yml");
+        Config = YamlConfiguration.loadConfiguration(file);
+
+        if (!pl.getDataFolder().exists()) {
+            pl.getDataFolder().mkdir();
+        }
+
+        create();
+    }
+
+    public static File getFile() {
+        return file;
+    }
+
+    public static YamlConfiguration getConfig() {
+        return Config;
+    }
+
+    public static void reloadConfig() {
+        loadConfig(pl);
+    }
+
+    public static void saveConfigFile() {
+        try {
+            Config.save(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void create() {
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Config.set("PlayerOption.Enable", Boolean.valueOf(true));
+            Config.set("PlayerOption.Help.Use_Permission", Boolean.valueOf(false));
+            Config.set("PlayerOption.DoubleJump.Enable", Boolean.valueOf(true));
+            Config.set("PlayerOption.DoubleJump.Use_Permission", Boolean.valueOf(false));
+            Config.set("PlayerOption.Fly.Enable", Boolean.valueOf(true));
+            Config.set("PlayerOption.Fly.Use_Permission", Boolean.valueOf(false));
+            Config.set("PlayerOption.Disable-Message", Boolean.valueOf(false));
+            
+            saveConfigFile();
+
+        }
+    }
+}
