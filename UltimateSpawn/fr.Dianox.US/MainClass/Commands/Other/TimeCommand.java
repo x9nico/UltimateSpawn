96ce4d10e6cc4f37e7886daa463fdfd8b404bace
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-22 00:59:29 +0200

    Update TimeCommand.java

--- UltimateSpawn/fr.Dianox.US/MainClass/Commands/Other/TimeCommand.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Commands/Other/TimeCommand.java
@@ -6,7 +6,10 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import fr.Dianox.US.MainClass.Utils.PlaceHolderMessageUtils;
 import fr.Dianox.US.MainClass.config.ConfigMessage;
+import fr.Dianox.US.MainClass.config.command.ConfigCWeatherTime;
+import fr.Dianox.US.MainClass.config.messages.ConfigMWeatherTime;
 
 public class TimeCommand implements CommandExecutor {
 	
@@ -23,40 +26,93 @@ public class TimeCommand implements CommandExecutor {
 		Player p = (Player) sender;
 		
 		if (cmd.getName().equalsIgnoreCase("night")) {
-			if (p.hasPermission("ultimatespawn.command.time.night")) {
-				p.getWorld().setTime(16000L);
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("day")) {
-			if (p.hasPermission("ultimatespawn.command.time.day")) {
-				p.getWorld().setTime(0L);
+			if (ConfigCWeatherTime.getConfig().getBoolean("Time.Set.Night.Enable")) {
+				if (p.hasPermission("ultimatespawn.command.time.night")) {
+					p.getWorld().setTime(ConfigCWeatherTime.getConfig().getLong("Time.Set.Night.Value"));
+					if (ConfigMWeatherTime.getConfig().getBoolean("Time.Set.Night.Enable")) {
+						for (String msg: ConfigMWeatherTime.getConfig().getStringList("Time.Set.Night.Message")) {
+							PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						}
+					}
+				}
+			} else {
+				if (ConfigCWeatherTime.getConfig().getBoolean("Time.Set.Night.Disable-Message")) {
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
+				}
 			}
 		}
 		
-		if (cmd.getName().equalsIgnoreCase("sun")) {
-			if (p.hasPermission("ultimatespawn.command.weather.sun")) {
-				p.getWorld().setThundering(false);
-				p.getWorld().setStorm(false);
+		if (cmd.getName().equalsIgnoreCase("day")) {
+			if (ConfigCWeatherTime.getConfig().getBoolean("Time.Set.Day.Enable")) {
+				if (p.hasPermission("ultimatespawn.command.time.day")) {
+					p.getWorld().setTime(ConfigCWeatherTime.getConfig().getLong("Time.Set.Day.Value"));
+					if (ConfigMWeatherTime.getConfig().getBoolean("Time.Set.Day.Enable")) {
+						for (String msg: ConfigMWeatherTime.getConfig().getStringList("Time.Set.Day.Message")) {
+							PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						}
+					}
+				}
+			} else {
+				if (ConfigCWeatherTime.getConfig().getBoolean("Time.Set.Day.Disable-Message")) {
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
+				}
+			}
+		}
+		
+		if (cmd.getName().equalsIgnoreCase("sun") || cmd.getName().equalsIgnoreCase("clear")) {
+			if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Sun.Enable")) {
+				if (p.hasPermission("ultimatespawn.command.weather.sun")) {
+					p.getWorld().setThundering(false);
+					p.getWorld().setStorm(false);
+					if (ConfigMWeatherTime.getConfig().getBoolean("Weather.Set.Sun.Enable")) {
+						for (String msg: ConfigMWeatherTime.getConfig().getStringList("Weather.Set.Sun.Message")) {
+							PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						}
+					}
+				}
+			} else {
+				if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Sun.Disable-Message")) {
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
+				}
 			}
 		}
 		
 		if (cmd.getName().equalsIgnoreCase("rain")) {
-			if (p.hasPermission("ultimatespawn.command.weather.rain")) {
-				p.getWorld().setThundering(false);
-				p.getWorld().setStorm(true);
+			if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Rain.Enable")) {
+				if (p.hasPermission("ultimatespawn.command.weather.rain")) {
+					p.getWorld().setThundering(false);
+					p.getWorld().setStorm(true);
+					if (ConfigMWeatherTime.getConfig().getBoolean("Weather.Set.Rain.Enable")) {
+						for (String msg: ConfigMWeatherTime.getConfig().getStringList("Weather.Set.Rain.Message")) {
+							PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						}
+					}
+				}
+			} else {
+				if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Rain.Disable-Message")) {
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
+				}
 			}
 		}
 		
 		if (cmd.getName().equalsIgnoreCase("thunder")) {
-			if (p.hasPermission("ultimatespawn.command.weather.thunder")) {
-				p.getWorld().setThundering(true);
-				p.getWorld().setStorm(true);
+			if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Thunder.Enable")) {
+				if (p.hasPermission("ultimatespawn.command.weather.thunder")) {
+					p.getWorld().setThundering(true);
+					p.getWorld().setStorm(true);
+					if (ConfigMWeatherTime.getConfig().getBoolean("Weather.Set.Thunder.Enable")) {
+						for (String msg: ConfigMWeatherTime.getConfig().getStringList("Weather.Set.Thunder.Message")) {
+							PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						}
+					}
+				}
+			} else {
+				if (ConfigCWeatherTime.getConfig().getBoolean("Weather.Set.Thunder.Disable-Message")) {
+					PlaceHolderMessageUtils.ReplaceCharMessagePlayer(ConfigMessage.getConfig().getString("Error.Command-disable"), p);
+				}
 			}
 		}
 		
 		return true;
 	}
-	
-	
-
 }
