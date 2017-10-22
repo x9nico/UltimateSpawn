acbb0924dbb156c376f488691bd6c155cc40dbd3
DianoxDragon <dianoxcontactpro@gmail.com>
2017-10-22 07:34:40 +0200

    Create Manager.java

---
+++ UltimateSpawn/fr.Dianox.US/MainClass/Manager.java
@@ -0,0 +1,34 @@
+package fr.Dianox.US.MainClass;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+
+import fr.Dianox.US.MainClass.event.BasicFeatures;
+import fr.Dianox.US.MainClass.event.ChangeWorldEvent;
+import fr.Dianox.US.MainClass.event.FunFeatures;
+import fr.Dianox.US.MainClass.event.LittlesEvent;
+import fr.Dianox.US.MainClass.event.OnChat;
+import fr.Dianox.US.MainClass.event.OnCommand;
+import fr.Dianox.US.MainClass.event.OnJoin;
+import fr.Dianox.US.MainClass.event.OnQuit;
+
+public class Manager {
+	
+	public MainClass pl;
+	
+	public Manager(MainClass us) {
+		this.pl = us;
+	}
+	
+	public void registerEvents() {
+		PluginManager pm = Bukkit.getPluginManager();
+		pm.registerEvents(new BasicFeatures(), pl);
+		pm.registerEvents(new OnJoin(), pl);
+		pm.registerEvents(new OnQuit(), pl);
+		pm.registerEvents(new OnChat(), pl);
+		pm.registerEvents(new FunFeatures(), pl);
+		pm.registerEvents(new LittlesEvent(), pl);
+		pm.registerEvents(new ChangeWorldEvent(), pl);
+		pm.registerEvents(new OnCommand(), pl);
+	}
+}
