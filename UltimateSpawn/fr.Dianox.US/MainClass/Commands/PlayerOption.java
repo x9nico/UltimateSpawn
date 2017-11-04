9240fda743d41f9d2d954f92069ab5b009336b5b
DianoxDragon <dianoxcontactpro@gmail.com>
2017-11-04 11:59:37 +0100

    Update PlayerOption.java

--- UltimateSpawn/fr.Dianox.US/MainClass/Commands/PlayerOption.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Commands/PlayerOption.java
@@ -7,6 +7,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import fr.Dianox.US.MainClass.Utils.PlaceHolderMessageUtils;
 import fr.Dianox.US.MainClass.config.ConfigTemp;
@@ -35,28 +37,18 @@ public class PlayerOption implements CommandExecutor {
 				 if (args.length == 0) {
 					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.Help.Use_Permission")) {
 						 if (p.hasPermission("ultimatespawn.command.playeroption.help")) {
-							 sender.sendMessage("§8//§7§m---------------§r§8\\\\ §3[§bPlayerOption§3] §8//§7§m---------------§r§8\\\\");
-			                 sender.sendMessage("");
-			                 sender.sendMessage("     §l>> §e§o§lPlayer option Help");
-			                 sender.sendMessage("");
-			                 sender.sendMessage(" §8>> §7/option fly - §eSet the fly");
-			                 sender.sendMessage(" §8>> §7/option doublejump - §eEnable or disable the doublejump");
-			                 sender.sendMessage("");
-			                 sender.sendMessage("§8\\\\§7§m---------------§r§8// §3[§bPlayerOption§3] §8\\\\§7§m---------------§r§8//");
+							 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Help")) {
+								 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+							 }
 						 } else {
 							 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.No-Permission")) {
 			                		PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
 							 }
 						 }
 					 } else {
-						 sender.sendMessage("§8//§7§m---------------§r§8\\\\ §3[§bPlayerOption§3] §8//§7§m---------------§r§8\\\\");
-		                 sender.sendMessage("");
-		                 sender.sendMessage("     §l>> §e§o§lPlayer option Help");
-		                 sender.sendMessage("");
-		                 sender.sendMessage(" §8>> §7/option fly - §eSet the fly");
-		                 sender.sendMessage(" §8>> §7/option doublejump - §eEnable or disable the doublejump");
-		                 sender.sendMessage("");
-		                 sender.sendMessage("§8\\\\§7§m---------------§r§8// §3[§bPlayerOption§3] §8\\\\§7§m---------------§r§8//");
+						 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Help")) {
+							 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+						 }
 					 }
 				 } else if (args[0].equalsIgnoreCase("fly")) {
 					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.Fly.Enable")) {
@@ -155,8 +147,41 @@ public class PlayerOption implements CommandExecutor {
 						 }
 					 }
 				 } else if (args[0].equalsIgnoreCase("doublejump") || args[0].equalsIgnoreCase("dj")) {
-					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.DoubleJump.Use_Permission")) {
-						 if (p.hasPermission("ultimatespawn.command.playeroption.doublejump")) {
+					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.DoubleJump.Enable")) {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.DoubleJump.Use_Permission")) {
+							 if (p.hasPermission("ultimatespawn.command.playeroption.doublejump")) {
+								 if (ConfigTemp.getConfig().getBoolean(pU+".Options.Fly.Enable")) {
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Error.DoubleJump")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+									 return true;
+								 }
+								 if (ConfigTemp.getConfig().getBoolean(pU+".Options.DoubleJump-Enable")) {
+									 ConfigTemp.getConfig().set(pU+".Options.DoubleJump-Enable", Boolean.valueOf(false));
+									 
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.DoubleJump.Disable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+									 p.setFlying(false);
+									 p.setAllowFlight(false);
+								 } else {
+									 p.setFlying(false);
+									 ConfigTemp.getConfig().set(pU+".Options.DoubleJump-Enable", Boolean.valueOf(true));
+									 
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.DoubleJump.Enable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+								 }
+							 } else {
+								 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.No-Permission")) {
+				                		PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+								 }
+							 }
+						 } else {
 							 if (ConfigTemp.getConfig().getBoolean(pU+".Options.Fly.Enable")) {
 								 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Error.DoubleJump")) {
 									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
@@ -183,35 +208,114 @@ public class PlayerOption implements CommandExecutor {
 									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
 								 }
 							 }
+						 }
+					 } else {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("Announce.Broadcast.Disable-Message")) {
+							 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.Command-disable")) {
+								 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+							 }
+						 }
+					 }
+				 } else if (args[0].equalsIgnoreCase("speed")) {
+					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.Speed.Enable")) {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.Speed.Use_Permission")) {
+							 if (p.hasPermission("ultimatespawn.command.playeroption.speed")) {
+								 if (ConfigTemp.getConfig().getBoolean(pU+".Options.Speed")) {
+									 p.removePotionEffect(PotionEffectType.SPEED);
+									 
+									 ConfigTemp.getConfig().set(pU+".Options.Speed", Boolean.valueOf(false));
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Speed.Disable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+								 } else {
+									 p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, ConfigCPlayerOption.getConfig().getInt("PlayerOption.Speed.Amplifier"), false, false));
+									 
+									 ConfigTemp.getConfig().set(pU+".Options.Speed", Boolean.valueOf(true));
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Speed.Enable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+								 }
+							 }
 						 } else {
-							 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.No-Permission")) {
-			                		PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+							 if (ConfigTemp.getConfig().getBoolean(pU+".Options.Speed")) {
+								 p.removePotionEffect(PotionEffectType.SPEED);
+								 
+								 ConfigTemp.getConfig().set(pU+".Options.Speed", Boolean.valueOf(false));
+								 ConfigTemp.saveConfigFile();
+								 
+								 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Speed.Disable")) {
+									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+								 }
+							 } else {
+								 p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, ConfigCPlayerOption.getConfig().getInt("PlayerOption.Speed.Amplifier"), false, false));
+								 
+								 ConfigTemp.getConfig().set(pU+".Options.Speed", Boolean.valueOf(true));
+								 ConfigTemp.saveConfigFile();
+								 
+								 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Speed.Enable")) {
+									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+								 }
 							 }
 						 }
 					 } else {
-						 if (ConfigTemp.getConfig().getBoolean(pU+".Options.Fly.Enable")) {
-							 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.Error.DoubleJump")) {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("Announce.Broadcast.Disable-Message")) {
+							 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.Command-disable")) {
 								 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
 							 }
-							 return true;
 						 }
-						 if (ConfigTemp.getConfig().getBoolean(pU+".Options.DoubleJump-Enable")) {
-							 ConfigTemp.getConfig().set(pU+".Options.DoubleJump-Enable", Boolean.valueOf(false));
-							 
-							 ConfigTemp.saveConfigFile();
-							 
-							 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.DoubleJump.Disable")) {
-								 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+					 }
+				 } else if (args[0].equalsIgnoreCase("jumpboost")) {
+					 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.JumpBoost.Enable")) {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("PlayerOption.JumpBoost.Use_Permission")) {
+							 if (p.hasPermission("ultimatespawn.command.playeroption.jumpboost")) {
+								 if (ConfigTemp.getConfig().getBoolean(pU+".Options.JumpBoost")) {
+									 p.removePotionEffect(PotionEffectType.JUMP);
+									 
+									 ConfigTemp.getConfig().set(pU+".Options.JumpBoost", Boolean.valueOf(false));
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.JumpBoost.Disable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+								 } else {
+									 p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999999, ConfigCPlayerOption.getConfig().getInt("PlayerOption.JumpBoost.Amplifier"), false, false));
+									 
+									 ConfigTemp.getConfig().set(pU+".Options.JumpBoost", Boolean.valueOf(true));
+									 ConfigTemp.saveConfigFile();
+									 
+									 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.JumpBoost.Enable")) {
+										 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+									 }
+								 }
 							 }
-							 p.setFlying(false);
-							 p.setAllowFlight(false);
 						 } else {
-							 p.setFlying(false);
-							 ConfigTemp.getConfig().set(pU+".Options.DoubleJump-Enable", Boolean.valueOf(true));
-							 
-							 ConfigTemp.saveConfigFile();
-							 
-							 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.DoubleJump.Enable")) {
+							 if (ConfigTemp.getConfig().getBoolean(pU+".Options.JumpBoost")) {
+								 p.removePotionEffect(PotionEffectType.JUMP);
+								 
+								 ConfigTemp.getConfig().set(pU+".Options.JumpBoost", Boolean.valueOf(false));
+								 ConfigTemp.saveConfigFile();
+								 
+								 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.JumpBoost.Disable")) {
+									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+								 }
+							 } else {
+								 p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999999, ConfigCPlayerOption.getConfig().getInt("PlayerOption.JumpBoost.Amplifier"), false, false));
+								 
+								 ConfigTemp.getConfig().set(pU+".Options.JumpBoost", Boolean.valueOf(true));
+								 ConfigTemp.saveConfigFile();
+								 
+								 for (String msg: ConfigMPlayerOption.getConfig().getStringList("PlayerOption.JumpBoost.Enable")) {
+									 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
+								 }
+							 }
+						 }
+					 } else {
+						 if (ConfigCPlayerOption.getConfig().getBoolean("Announce.Broadcast.Disable-Message")) {
+							 for (String msg: ConfigMPlugin.getConfig().getStringList("Error.Command-disable")) {
 								 PlaceHolderMessageUtils.ReplaceCharMessagePlayer(msg, p);
 							 }
 						 }
