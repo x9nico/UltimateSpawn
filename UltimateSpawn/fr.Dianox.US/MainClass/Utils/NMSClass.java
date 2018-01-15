b04fe4035005ffc3af039ed48458df8ad1bb77fc
x9nico <x9nico2@outlook.fr>
2018-01-15 15:06:55 +0100

    Added Method for Mount System (Soon)

--- UltimateSpawn/fr.Dianox.US/MainClass/Utils/NMSClass.java
+++ UltimateSpawn/fr.Dianox.US/MainClass/Utils/NMSClass.java
@@ -4,6 +4,55 @@ import org.bukkit.Bukkit;
 
 public class NMSClass {
 	
+	/**
+	Registering custom Entities (can be used for Mount System)
+	*/
+	public static void registerEntity(String name, int id, Class<? extends EntityInsentient> nmsClass, Class<? extends EntityInsentient> customClass) {
+        try {
+
+            List<Map<?, ?>> dataMaps = new ArrayList<Map<?, ?>>();
+            for (Field f : EntityTypes.class.getDeclaredFields()) {
+                if (f.getType().getSimpleName().equals(Map.class.getSimpleName())) {
+                    f.setAccessible(true);
+                    dataMaps.add((Map<?, ?>) f.get(null));
+                }
+            }
+
+            if (dataMaps.get(2).containsKey(id)) {
+                dataMaps.get(0).remove(name);
+                dataMaps.get(2).remove(id);
+            }
+
+            Method method = EntityTypes.class.getDeclaredMethod("a", Class.class, String.class, int.class);
+            method.setAccessible(true);
+            method.invoke(null, customClass, name, id);
+
+            for (Field f : BiomeBase.class.getDeclaredFields()) {
+                if (f.getType().getSimpleName().equals(BiomeBase.class.getSimpleName())) {
+                    if (f.get(null) != null) {
+
+                        for (Field list : BiomeBase.class.getDeclaredFields()) {
+                            if (list.getType().getSimpleName().equals(List.class.getSimpleName())) {
+                                list.setAccessible(true);
+                                @SuppressWarnings("unchecked")
+                                List<BiomeMeta> metaList = (List<BiomeMeta>) list.get(f.get(null));
+    
+                                for (BiomeMeta meta : metaList) {
+                                    Field clazz = BiomeMeta.class.getDeclaredFields()[0];
+                                    if (clazz.get(meta).equals(nmsClass)) {
+                                        clazz.set(meta, customClass);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
 	  public static Class<?> getNMSClass(String name) {
 		    String version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
 		    try {
